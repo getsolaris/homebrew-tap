7671df9853774f1926201e1d6804776f0a1dae02
getsolaris <getsolaris.kr@gmail.com>
2026-04-02 16:22:44 +0900

    fix: hide dylib from Homebrew relinking to avoid Mach-O header overflow

--- Formula/oh-my-worktree.rb
+++ Formula/oh-my-worktree.rb
@@ -12,7 +12,21 @@ class OhMyWorktree < Formula
 
   def install
     system "bun", "install"
+
+    dylib_relative = "node_modules/@opentui/core-darwin-arm64/libopentui.dylib"
+    dylib_backup = Pathname.new(Dir.tmpdir)/"omw-libopentui.dylib"
+
+    if File.exist?(dylib_relative)
+      FileUtils.mv dylib_relative, dylib_backup
+    end
+
     libexec.install Dir["*"]
+
+    if dylib_backup.exist?
+      target = libexec/dylib_relative
+      FileUtils.mv dylib_backup, target
+    end
+
     (bin/"omw").write_env_script libexec/"src/index.ts", PATH: "#{Formula["oven-sh/bun/bun"].opt_bin}:${PATH}"
   end
 
